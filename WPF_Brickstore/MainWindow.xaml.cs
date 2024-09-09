800b796d72fa862e274ee0fbccc50e31bf58f20c
hunor64 <nhunor06@gmail.com>
2024-09-09 13:45:39 +0200

    Added new filter

--- WPF_Brickstore/MainWindow.xaml.cs
+++ WPF_Brickstore/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,7 @@ namespace WPF_Brickstore
     {
         List<BrickedData> brickData = new();
         List<BrickedData> currentFilter = new();
+        ObservableCollection<string> cmbContent = new();
 
         public MainWindow()
         {
@@ -45,11 +47,13 @@ namespace WPF_Brickstore
                     BrickedData brick = new BrickedData(itemId, itemName, categoryName, colorName, qty);
                     brickData.Add(brick);
                 }
+                currentFilter = brickData;
                 dgBrickstore.ItemsSource = brickData;
             }
+            LoadCMBItems();
 
         }
-        public void Filter(object sender, TextChangedEventArgs e)
+        public void Filter()
         {
             currentFilter = brickData.Where(x => x.itemName.ToLower().Contains(txtFilter.Text.ToLower()) && x.categoryName.ToLower().Contains(cmbFilterKat.Text.ToLower()) && x.itemId.Contains(txtFilterKod.Text.ToLower())).ToList();
             dgBrickstore.ItemsSource = currentFilter;
@@ -57,7 +61,35 @@ namespace WPF_Brickstore
         }
         public void LoadCMBItems()
         {
+            cmbContent = [];
+            currentFilter.ForEach(x =>
+            {
+                if (x.categoryName.Contains(",") && cmbContent.Contains(x.categoryName.Split(',')[1]) == false)
+                {
+                    cmbContent.Add(x.categoryName.Split(',')[1].Trim());
+                }
+                if (cmbContent.Contains(x.categoryName) == false)
+                {
+                    if (x.categoryName.Contains(",") == false)
+                    {
+                        cmbContent.Add(x.categoryName.Trim());
+                    }
+                }
+                
+            });
 
+            cmbFilterKat.ItemsSource = cmbContent.Distinct().OrderBy(x => x);
+        }
+
+        private void cmbFilterKat_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void Filter(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+            LoadCMBItems();
         }
     }
 }
